e09c744cd481e6041033720530e995419b7f7b67
maxhuizing2001 <maxhuizing@gmail.com>
2022-12-20 13:30:23 +0100

    jopie

---
+++ c++/a/main.cpp
@@ -0,0 +1,12 @@
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+float some_fn(float arg1, float arg2){
+    return arg1 + arg2;
+}
+
+PYBIND11_MODULE(module_name, handle){
+    handle.doc() = "hehe";
+    handle.def("some_fn_python_name", &some_fn);
+}
